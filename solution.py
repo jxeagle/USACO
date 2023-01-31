341b9236bfbd9436fad06b2668ce21c09c88b6ae
Jack Xu <jxu@eagleinvsys.com>
2023-01-31 16:43:03 -0500

    initial

---
+++ solution.py
@@ -0,0 +1,3 @@
+def perimeter(cows, connections):
+    ...
+
---
+++ tests/test_solutions.py
@@ -0,0 +1,8 @@
+from solution import perimeter
+
+def test_solution():
+    # read the input.txt file
+    with open('tests/input.txt', 'r') as f:
+        # read the first line
+        line = f.readline()
+        print(line)
